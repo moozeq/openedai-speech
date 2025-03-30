37390db150ec8a73d6086ecf3f3648f399561ea1
moozeq <comical_pupal.0p@icloud.com>
2025-03-31 01:45:50 +0200

    feat: remove unnecessary commands

--- startup.sh
+++ startup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-[ -f speech.env ] && . speech.env
-
-echo "First startup may download 2GB of speech models. Please wait."
-
-bash download_voices_tts-1.sh
-bash download_voices_tts-1-hd.sh $PRELOAD_MODEL
-
-python speech.py ${PRELOAD_MODEL:+--preload $PRELOAD_MODEL} $EXTRA_ARGS $@
+python speech.py
